5267c769893183814f7569c9d55046ee6f82e408
afaq.ansari <afaq.ansari@flixbus.com>
2018-11-05 02:09:07 +0100

    Change s3 key for input to spark. Ignore target folders for commit.

--- src/main/scala/com/aaa/SparkLocalToS3.scala
+++ src/main/scala/com/aaa/SparkLocalToS3.scala
@@ -4,10 +4,10 @@ object SparkLocalToS3 extends App {
   SparkUtils.withSpark { sparkSession =>
     // Reading file from S3 directly without writing extra code to download files from S3
     val df = sparkSession.sqlContext.read
-      .parquet("s3a://some-bucket-name/some-path/some-file.snappy.parquet")
+      .parquet("s3a://some-bucket-name/some-in-path/parquet")
     df.show()
 
     // Writing output to S3 directly without writing extra code to upload files
-    df.write.csv("s3a://some-bucket-name/spark_local_to_s3/csv")
+    df.write.csv("s3a://some-bucket-name/some-out-path/csv")
   }
 }
